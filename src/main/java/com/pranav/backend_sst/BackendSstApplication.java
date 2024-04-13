72d3461307584721faf82221e350a0e6ed3b38a8
Pranav Aggarwal <pranavaggarwal1100@gmail.com>
2024-04-14 01:16:02 +0530

    Your commit message

---
+++ src/main/java/com/pranav/backend_sst/BackendSstApplication.java
@@ -0,0 +1,13 @@
+package com.pranav.backend_sst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pranav/backend_sst/BackendSstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranav.backend_sst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
