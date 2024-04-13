b1517795fc2b23e4dd48656d68a904aa57e560a1
Pranav Aggarwal <pranavaggarwal1100@gmail.com>
2024-04-14 01:49:50 +0530

    Models_Complete

---
+++ src/main/java/com/pranav/backend_sst/Models/Category.java
@@ -0,0 +1,14 @@
+package com.pranav.backend_sst.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Category
+{
+    private Long id;
+    private String title;
+    private String description;
+}
